633d728ae3d74232a3d28a6a7396fdc5f248b70e
Anna <anyaa.vogel@gmail.com>
2023-02-15 00:22:29 +0300

    seminar 1 po c#

---
+++ task0/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+int A = 5;
+int B = 7;
+int C = 2;
+int d = 10;
+int e = -9;
+int f = -3;
+int max1 = A;
+int max2 = C;
+int max3 = e;
+if (A>max1) max1 = A;
+if (B>max1) max1 = B;
+if (C>max2) max2 = C;
+if (d>max2) max2 = d;
+if (e>max3) max3 =d;
+if (f>max3) max3=f;
+Console.Write ("max1=");
+Console.WriteLine (max1);
+Console.Write ("max2=");
+Console.WriteLine (max2);
+Console.Write ("max3=");
+Console.WriteLine (max3);
---
+++ task1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine ("Введите число 1: ");
+int numb1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 2: ");
+int numb2 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 3: ");
+int numb3 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 4: ");
+int numb4 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 5: ");
+int numb5 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 6: ");
+int numb6 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 7: ");
+int numb7 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 8: ");
+int numb8 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите число 9: ");
+int numb9 = Convert.ToInt32 (Console.ReadLine ());
+
+int max1 = numb1;
+int max2 = numb4;
+int max3 = numb7;
+if (numb1>max1) max1 = numb1;
+if (numb2>max1) max1 = numb2;
+if (numb3>max1) max1 = numb3;
+if (numb4>max2) max2 = numb4;
+if (numb5>max2) max2 = numb5;
+if (numb6>max2) max2 = numb6;
+if (numb7>max3) max3 = numb7;
+if (numb8>max3) max3 = numb8;
+if (numb9>max3) max3 = numb9;
+Console.Write ("max1=");
+Console.WriteLine (max1);
+Console.Write ("max2=");
+Console.WriteLine (max2);
+Console.Write ("max3=");
+Console.WriteLine (max3);
+
